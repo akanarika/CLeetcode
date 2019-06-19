21c89aaa86369320cfcc1b6173405643c25f82b0
akanarika <ilykn@hotmail.com>
2019-06-19 00:03:09 -0700

    521 added

---
+++ 521. Longest Uncommon Subsequence I.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        int len = 0;
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > len; j--) {
+                if (b.find(a.substr(i, j)) == string::npos) {
+                    len = max(len, j);
+                    break;
+                } 
+            }
+        }
+        
+        for (int i = 0; i < b.length(); i++) {
+            for (int j = b.length() - i; j > len; j--) {
+                if (a.find(b.substr(i, j)) == string::npos) {
+                    len = max(len, j);
+                    break;
+                } 
+            }
+        }
+        
+        return (len == 0)? -1 : len;
+    }
+};
