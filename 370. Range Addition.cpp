9444359f8a285eda231b39aa3866af2e7d784252
dandandu <ilykn@hotmail.com>
2019-09-06 00:09:36 -0700

    370 added

---
+++ 370. Range Addition.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getModifiedArray(int length, vector<vector<int>>& updates) { 
+        vector<int> arr(length, 0);
+        for (auto update : updates) {
+            arr[update[0]] += update[2];
+            if (update[1] < length - 1) arr[update[1] + 1] -= update[2];
+        }
+        
+        for (int i = 1; i < length; i++) {
+            arr[i] += arr[i - 1];
+        }
+        
+        return arr;
+    }
+};
