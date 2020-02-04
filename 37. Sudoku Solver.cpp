b0fb35a55a0e424d252a23eb45cde38d985d4370
chaoying_du <ilykn@hotmail.com>
2020-02-04 09:59:40 -0800

    37 added

--- 37. Sudoku Solver.cpp
+++ 37. Sudoku Solver.cpp
@@ -8,82 +8,52 @@
 class Solution {
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        /**
-        unordered_set<int> defaultSet = {1,2,3,4,5,6,7,8,9};
-        vector<vector<unordered_set<int>>> possibleNumbers(9, vector<unordered_set<int>>(9, defaultSet));
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] != '.') {
-                    int num = board[row][col] - '0';
-                    for (int c = 0; c < 9; c++) {
-                        if (board[row][c] == '.') possibleNumbers[row][c].erase(num);
-                    }
-                    for (int r = 0; r < 9; r++) {
-                        if (board[r][col] == '.') possibleNumbers[r][col].erase(num);
-                    }
-                    int blockR = row / 3;
-                    int blockC = col / 3;
-                    for (int br = 0; br < 3; br++) {
-                        for (int bc = 0; bc < 3; bc++) {
-                            if (board[blockR + br][blockC + bc] == '.') possibleNumbers[blockR + br][blockC + bc].erase(num);
-                        }
-                    }
-                    possibleNumbers[row][col].clear();
-                    possibleNumbers[row][col].insert(num);
-                }
-            }
-        }
-        solve(0, 0, possibleNumbers);
-        **/
-        vector<vector<unordered_set<int>>> poNum;
-        solve(0, 1, poNum, board);
-    }
-
-    bool solve(int i, int num, vector<vector<unordered_set<int>>>& poNum, vector<vector<char>>& board) {
-        int row = i / 9;
-        int col = i % 9;
-        if (board[row][col] != '.') {
-            if (i == 80) return true;
-            return solve(i + 1, 1, poNum, board);
-        } else {
-            board[row][col] = (char)(num + '0');
-            if (check(row, col, num, board)) {
-                if (i == 80) return true;
-                return solve(i + 1, 1, poNum, board);
-            } else {
-                bool can = solve(i, num + 1, poNum, board);
-                if (can) return true;
-                else {
-                    board[row][col] = '.';
-                    return solve(i, num + 1, poNum, board);
-                }
-            }
-        }
-    }
-
-    bool check(int row, int col, int num, vector<vector<char>>& board) {
-        cout << "[" << row << "," << col << "]" << ": " << num << ";";
-        for (int c = 0; c < 9; c++) {
-            if (c == col) continue;
-            if (board[row][c] == '.') continue;
-            if ((board[row][c] - '0') == num) return false;
-        }
+        rows = vector<vector<int>>(9, vector<int>(10, 0));
+        cols = vector<vector<int>>(9, vector<int>(10, 0));
+        blks = vector<vector<int>>(9, vector<int>(10, 0));
 
         for (int r = 0; r < 9; r++) {
-            if (r == row) continue;
-            if (board[r][col] == '.') continue;
-            if ((board[r][col] - '0') == num) return false;
-        }
-        int blockR = row / 3;
-        int blockC = col / 3;
-        for (int br = 0; br < 3; br++) {
-            for (int bc = 0; bc < 3; bc++) {
-                if ((blockR + br == row) && (blockC + bc == col)) continue;
-                if (board[blockR + br][blockC + bc] == '.') continue;
-                if ((board[blockR + br][blockC + bc] - '0') == num) return false;
+            for (int c = 0; c < 9; c++) {
+                int b = (r / 3) * 3 + c / 3;
+                if (board[r][c] != '.') {
+                    int num = board[r][c] - '0';
+                    rows[r][num] = 1;
+                    cols[c][num] = 1;
+                    blks[b][num] = 1;
+                }
             }
         }
-        return true;
+        solve(0, board);
     }
+
+private:
+    vector<vector<int>> rows;
+    vector<vector<int>> cols;
+    vector<vector<int>> blks;
+    bool solve(int i, vector<vector<char>>& board) {
+        int r = i / 9;
+        int c = i % 9;
+        int b = (r / 3) * 3 + c / 3;
+        if (board[r][c] != '.') {
+            if (i == 80) return true;
+            return solve(i + 1, board);
+        }
+
+        for (int num = 1; num <= 9; num++) {
+            if ((!rows[r][num]) && (!cols[c][num]) && (!blks[b][num])) {
+                board[r][c] = (char)('0' + num);
+                rows[r][num] = 1;
+                cols[c][num] = 1;
+                blks[b][num] = 1;
+                if (i == 80 || solve(i + 1, board)) return true;
+                board[r][c] = '.';
+                rows[r][num] = 0;
+                cols[c][num] = 0;
+                blks[b][num] = 0;
+            }
+        }
+        return false;
+    }
+
 };
-// @lc code=end
+    // @lc code=end
